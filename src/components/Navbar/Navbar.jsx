9ea25c1e6b683942407a61ec6b88fda57fe36103
emon-101 <emon26.ugv@gmail.com>
2026-03-29 22:08:47 +0600

    Update Navbar feature with shopping cart and login button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
 const Navbar = () => {
   return (
@@ -44,29 +45,39 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <a className="text-3xl lg:text-4xl font-bold bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent leading-tight whitespace-nowrap">DigiTools</a>
+          <a className="text-3xl lg:text-4xl font-bold bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent leading-tight whitespace-nowrap">
+            DigiTools
+          </a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 font-semibold text-[#101727]">
             <li>
-                <a>Products</a>
-              </li>
-              <li>
-                <a>Features</a>
-              </li>
-              <li>
-                <a>Pricing</a>
-              </li>
-              <li>
-                <a>Testimonials</a>
-              </li>
-              <li>
-                <a>FAQ</a>
-              </li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] px-5 py-2 rounded-full text-white transition-all hover:grayscale-50 font-medium">Get Started</a>
+        <div className="navbar-end flex gap-4 items-center">
+          <a href="">
+            <HiOutlineShoppingCart className="text-xl font-semibold text-[#101727]" />
+          </a>
+          <a className="font-semibold text-[#101727]" href="">
+            Login
+          </a>
+          <a className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] px-5 py-2 rounded-full text-white transition-all hover:grayscale-50 font-medium">
+            Get Started
+          </a>
         </div>
       </div>
     </div>
